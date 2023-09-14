2e4734746eb8baca0adb8b99e8a08341e97f5f17
Gokul Sujan <gokulsujan03@gmail.com>
2023-09-14 12:16:07 +0530

    Git ignore file addedd and db connection configuration done

---
+++ config/database.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
